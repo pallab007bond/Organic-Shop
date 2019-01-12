2359e6706e69e529a3bcebc5e751547e8866e695
Pallab Banerjee <rik.pallab@gmail.com>
2019-01-12 13:42:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyCgrkF_-l1KZ_j_KHkALdukNdY2DUltRy4",
-    authDomain: "oshop-24381.firebaseapp.com",
-    databaseURL: "https://oshop-24381.firebaseio.com",
-    projectId: "oshop-24381",
-    storageBucket: "oshop-24381.appspot.com",
-    messagingSenderId: "592173697301"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
