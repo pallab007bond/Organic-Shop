a5cba6a5673ec757fc63ce329520f4a983a49ce0
pallab banerjee <rik.pallab@gmail.com>
2019-01-12 13:05:02 +0530

    Added  bootstrap navigation bar

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
